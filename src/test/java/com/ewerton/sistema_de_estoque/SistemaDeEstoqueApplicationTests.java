509f8c0ea4bf0db8dc3ff9802e923e45abd191ab
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-15 19:13:55 -0300

    Primeiro commit do projeto

---
+++ src/test/java/com/ewerton/sistema_de_estoque/SistemaDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewerton.sistema_de_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
