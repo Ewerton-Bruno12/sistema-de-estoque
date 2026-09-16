9ddcca47f916b49295a765d3dd224db364b6747e
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-16 19:35:09 -0300

    feat: cria CategoryController com endpoints

---
+++ src/main/java/com/ewerton/sistema_de_estoque/controller/CategoryController.java
@@ -0,0 +1,51 @@
+package com.ewerton.sistema_de_estoque.controller;
+
+import com.ewerton.sistema_de_estoque.dto.CategoryRequestDto;
+import com.ewerton.sistema_de_estoque.dto.CategoryResponseDto;
+import com.ewerton.sistema_de_estoque.service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/categories")
+@RequiredArgsConstructor
+@Validated
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryResponseDto createCategory(@Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+        return categoryService.save(categoryRequestDto);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CategoryResponseDto> findAllCategories() {
+        return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryResponseDto findByIdCategory(@PathVariable Long id) {
+        return categoryService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryResponseDto updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+        return categoryService.update(id, categoryRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.delete(id);
+    }
+}
